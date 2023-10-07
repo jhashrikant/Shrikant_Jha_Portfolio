5a93c9d36cfa7a2c3738ba71404cd603df92ab97
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2023-10-07 19:56:37 +0530

    changed button

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,19 +21,4 @@ module.exports = {
   ],
 }
 
-// /**
-// //  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
-// //  */
-// module.exports = {
-//   content: [
-//     "./node_modules/flowbite-react/**/*.js",
-//     "./pages/**/*.{ts,tsx}",
-//     "./public/**/*.html",
-//   ],
-//   plugins: [
-//     require("flowbite/plugin")
-//   ],
-//   theme: {},
-// };
-
 
