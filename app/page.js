110cc0f63a866dc7793a3c6af194466e817837d6
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2023-10-07 19:40:03 +0530

    removed dependencies

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 // 'use client'
 
 // import Header from '@/Components/Header'
-import Image from 'next/image'
-import Homesection from '@/Components/sections/Homesection'
-// import Footer from '@/Components/Footer'
-import Projects from '@/Components/sections/Projects'
-import Skills from '@/Components/sections/Skills'
-import { TechnicalSkills } from '@/Components/utils/data'
+// import Image from 'next/image'
+// import Homesection from '@/Components/sections/Homesection'
+// // import Footer from '@/Components/Footer'
+// import Projects from '@/Components/sections/Projects'
+// import Skills from '@/Components/sections/Skills'
+// import { TechnicalSkills } from '@/Components/utils/data'
 // import { useEffect, useState } from 'react'
 
 export default function Home() {
