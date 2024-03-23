513f61748f47a6332aee165f5df72ef253d79e9a
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2024-03-23 22:28:22 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
 
 import './globals.css'
-// import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '@/Components/Header';
 import Footer from '@/Components/Footer';
 
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Shrikant Portfolio',
@@ -13,9 +11,6 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-
-
-
 
   return (
     <html lang="en">
--- app/page.js
+++ app/page.js
@@ -1,21 +1,10 @@
-// 'use client'
 
-// import Header from '@/Components/Header'
-// import Image from 'next/image'
-// import Homesection from '@/Components/sections/Homesection'
-// // import Footer from '@/Components/Footer'
-// import Projects from '@/Components/sections/Projects'
-// import Skills from '@/Components/sections/Skills'
-// import { TechnicalSkills } from '@/Components/utils/data'
-// import { useEffect, useState } from 'react'
 
 export default function Home() {
 
 
     return (
-        <>
-            {/* <Header />
-            <Footer /> */}
-        </>
+       <>
+       </>
     )
 }
